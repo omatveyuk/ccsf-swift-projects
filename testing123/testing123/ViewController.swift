41f9097e96b277c43dd4478c0f92601cef330ab4
omatveyuk <oxana@mishina.com>
2015-04-24 06:46:32 -0700

    e
    e

---
+++ testing123/testing123/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testing123
+//
+//  Created by misha birman1 on 4/24/15.
+//  Copyright (c) 2015 misha birman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
