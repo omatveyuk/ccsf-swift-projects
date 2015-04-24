9bf6af2dd3aa5a47e9bdf1b3b6df0617a0cafc53
omatveyuk <oxana@mishina.com>
2015-04-24 06:50:05 -0700

    add
    add

---
+++ SF Parking Helper/SF Parking Helper/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  SF Parking Helper
+//
+//  Created by misha birman1 on 4/24/15.
+//  Copyright (c) 2015 misha birman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var today = NSDate()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let myCalendar = NSCalendar.currentCalendar() // (calendarIdentifier: NSCalendarIdentifierGregorian)
+        
+        //let myComponents = myCalendar!.components(NSCalendarUnit.WeekOfMonthCalendarUnit, fromDate: today)
+        let myComponents = myCalendar.components(NSCalendarUnit.CalendarUnitHour | NSCalendarUnit.CalendarUnitDay | NSCalendarUnit.CalendarUnitWeekOfMonth, fromDate: today)
+        
+        let weekDay = myComponents.weekOfMonth
+        println((myComponents.day - 1 ) / 7 + 1 )
+        
+        println(weekDay)
+        println(myComponents.hour)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
